7294f35b266bc16f29fbebb620465c23d646782c
kodai <kodai@kodainoMacBook-Pro.local>
2021-02-19 19:38:48 +0900

    create-email-address-check-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'views#top'
-  get 'email-address-check' => 'users#email_address_check'
-  get 'signin' => 'users#signin'
+  get 'signin' => 'email_verification_codes#new'
+  post 'email_verification_codes' => 'email_verification_codes#create'
+  post 'verification' => 'email_verification_codes#verification'
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
